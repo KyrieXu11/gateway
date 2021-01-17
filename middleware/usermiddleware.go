994cf1b44b4f91da59344a6c87fe1f305a7ac819
KyrieXu <kyriexu11@126.com>
2021-01-17 23:30:26 +0800

    增加路径校验的TODO

--- middleware/usermiddleware.go
+++ middleware/usermiddleware.go
@@ -28,6 +28,10 @@ func CheckLogin() gin.HandlerFunc {
 // 检查是否为登录请求的函数
 func CheckLoginRequest(r *http.Request) bool {
 	// TODO: 做登陆路径的校验
+	// 介于golang里面没有类似spring中的 AntPathMatcher
+	// 考虑下面两种做法
+	// 1. 自己照着spring的源码写一个
+	// 2. 做一个 rpc 调用
 	realPath := r.URL.Path
 	log.Info(realPath)
 	return false
