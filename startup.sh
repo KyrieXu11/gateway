ebe21e83f2f6731ae7f17a35488cc4d736d0059d
KyrieXu <kyriexu11@126.com>
2021-01-29 00:47:12 +0800

    添加一键启动的脚本 未测试

---
+++ startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd java_service
+mvn package clean -Dmaven.test.skip=true
+mvn package -Dmaven.test.skip=true
+java -jar target/gateway_java_service-1.0-SNAPSHOT.jar >/data/logs/gateway/java_service_startup.log 2>&1 &
+cd ../go_service
+go run main.go > /data/logs/gateway/go_service_startup.log 2>&1 &
