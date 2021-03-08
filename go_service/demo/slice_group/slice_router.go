246450854ce2b1aa5452283482376d86ac976262
KyrieXu <kyriexu11@126.com>
2021-03-08 17:01:24 +0800

    add go_service middleware demo

--- go_service/demo/slice_group/slice_router.go
+++ go_service/demo/slice_group/slice_router.go
@@ -1,51 +1,139 @@
 package main
 
 import (
-	"context"
+	"log"
 	"net/http"
+	"sync"
 )
 
-type SliceRouter struct {
-	group []*SliceGroup
+type HandlerFunc func(c *Context)
+
+type HandlerChain []HandlerFunc
+
+type RouterGroup struct {
+	Handlers HandlerChain
+	engine   *Engine
 }
 
-type SliceGroup struct {
-	*SliceRouter
-	path     string
-	handlers []HandlerFunc
+type Engine struct {
+	tree map[string]HandlerChain
+	RouterGroup
+	pool sync.Pool
 }
 
-type SliceContext struct {
-	Rw  http.ResponseWriter
-	Req *http.Request
-	Ctx context.Context
-	*SliceGroup
-	index int8
+type Context struct {
+	Rw       http.ResponseWriter
+	Req      *http.Request
+	handlers HandlerChain
+	index    int8
 }
 
-type HandlerFunc func()
-
-func NewSliceRouter() *SliceRouter {
-	return &SliceRouter{}
+func NewEngine() *Engine {
+	engine := &Engine{
+		RouterGroup: RouterGroup{
+			Handlers: nil,
+		},
+		tree: make(map[string]HandlerChain),
+	}
+	engine.RouterGroup.engine = engine
+	engine.pool.New = func() interface{} {
+		return engine.allocateContext()
+	}
+	return engine
 }
 
-func (p *SliceRouter) Group(path string) *SliceGroup {
-	return &SliceGroup{
-		path:        path,
-		SliceRouter: p,
+func (c *Context) Next() {
+	c.index++
+	for c.index < int8(len(c.handlers)) {
+		c.handlers[c.index](c)
+		c.index++
 	}
 }
 
-func (p *SliceGroup) Use(middleware ...HandlerFunc) *SliceGroup {
-	p.handlers = append(p.handlers, middleware...)
-	exist := false
-	for _, group := range p.SliceRouter.group {
-		if group == p {
-			exist = true
-		}
+func (c *Context) Reset() {
+	c.handlers = nil
+	c.index = -1
+}
+
+func (engine *Engine) allocateContext() *Context {
+	return &Context{}
+}
+
+func (engine *Engine) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	c := engine.pool.Get().(*Context)
+	c.Rw = rw
+	c.Req = req
+	c.Reset()
+	engine.serveHttp(c)
+}
+
+func (engine *Engine) serveHttp(c *Context) {
+	rPath := c.Req.URL.Path
+	handlers := engine.getHandlers(rPath)
+	if handlers != nil {
+		c.handlers = handlers
+		c.Next()
 	}
-	if exist {
-		p.SliceRouter.group = append(p.SliceRouter.group, p)
+}
+
+func (engine *Engine) addRoute(path string, handlers HandlerChain) {
+	engine.tree[path] = handlers
+}
+
+func (engine *Engine) getHandlers(path string) (handlers HandlerChain) {
+	handlers, ok := engine.tree[path]
+	if !ok {
+		return nil
 	}
-	return p
+	return
+}
+
+func (group *RouterGroup) Use(middleware ...HandlerFunc) {
+	group.Handlers = append(group.Handlers, middleware...)
+}
+
+// set common middleware
+func (engine *Engine) Use(middleware ...HandlerFunc) {
+	engine.RouterGroup.Use(middleware...)
+}
+
+// specific middleware
+func (group *RouterGroup) AddRoute(absolutePath string, handlers ...HandlerFunc) {
+	handlers = group.combineHandlers(handlers)
+	group.engine.addRoute(absolutePath, handlers)
+}
+
+// merge specific and common middleware
+func (group *RouterGroup) combineHandlers(handlers HandlerChain) HandlerChain {
+	finalSize := len(group.Handlers) + len(handlers)
+	mergedHandlers := make(HandlerChain, finalSize)
+	copy(mergedHandlers, group.Handlers)
+	copy(mergedHandlers[len(group.Handlers):], handlers)
+	return mergedHandlers
+}
+
+func main() {
+	engine := NewEngine()
+	engine.Use(func(c *Context) {
+		c.Rw.Write([]byte("first middleware\n"))
+		c.Next()
+		log.Println("end first middleware")
+	})
+	engine.Use(func(c *Context) {
+		c.Rw.Write([]byte("second middleware\n"))
+		c.Next()
+		log.Println("end second middleware")
+	})
+	engine.Use(func(c *Context) {
+		c.Rw.Write([]byte("third middleware\n"))
+		c.Next()
+		log.Println("end third middleware")
+	})
+	engine.AddRoute("/v1/test", func(c *Context) {
+		s := "this is a specific middleware"
+		c.Rw.Write([]byte(s))
+		log.Println(s)
+		c.Next()
+	})
+	http.ListenAndServe(":8081", engine)
 }
